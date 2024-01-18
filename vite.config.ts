dfb940ecf9a0e330738e7f138395f50289f03c4d
octapf <frangipani.octavio@gmail.com>
2024-01-18 01:48:51 +0100

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
 	const config = {
 		plugins: [react()],
 		base: '/',
-	}
-
-	if (command !== 'serve') {
-		config.base = '/ticket-scanner-ui/'
 	}
 
 	return config
